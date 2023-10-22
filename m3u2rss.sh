8d2a3912d1c350a052ff3f9eb47126fc430d594e
Erik Martino <martino@chainalysis.com>
2023-10-22 18:22:48 +0200

    icon

--- m3u2rss.sh
+++ m3u2rss.sh
@@ -13,15 +13,15 @@ cat <<EOF
 <?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
 <channel>
- <title>RSS Title</title>
- <description>This is an example of an RSS feed</description>
+ <title>HHGTTG</title>
+ <description>HHGTTG</description>
  <language>en-us</language>
  <copyright>bbc</copyright>
- <link>http://www.example.com/main.html</link>
+ <link>https://erikmartino.github.io/</link>
  <image>
-   <url>http://www.example.com/favicon.png</url>
-   <link>http://www.example.com/main.html</link>
-   <title>favicon</title>
+   <url>https://erikmartino.github.io/favicon.png</url>
+   <link>https://erikmartino.github.io/</link>
+   <title>HHGTTG</title>
  </image>
  <generator>m3u2rss.sh</generator>
  <ttl>263520</ttl>
@@ -54,7 +54,7 @@ while read l; do
 	cat <<END
 	<item>
 		<link>$l</link>
-		<guid>$(uuidgen)</guid>
+		<guid>$l</guid>
 		<description>$(echo $l | sed -e 's!.*/!!' -e 's/%20/ /g' -e 's/.mp3$//')</description>
 		<pubDate>$(date -R)</pubDate>
 		<enclosure url="$l" type="audio/mpeg" length="$(curl -sLI $l | grep Content-Length: | awk '{print $2}' | tr -d '\r\n')" />
